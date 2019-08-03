b60bab661b0d43580438ce424cc19e3d03b2ff13
Leo.Huang <464362353@qq.com>
2019-08-03 18:37:48 +0800

    :package: 发了个包.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'lib'),
     filename: 'Hmmmmmm.js',
     library: 'Hmmmmmm',
     libraryTarget: 'umd',
